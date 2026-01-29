299d275887819b3788d8ddbac7fb57c1031f20bb
abhinav17reja <abhinav17reja@gmail.com>
2026-01-29 16:28:34 -0700

    Fixed the design

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -645,7 +645,7 @@ export default function VideoCard(props: VideoCardProps) {
   }, [creatorId, postId, router]);
 
   return (
-    <div className="relative w-full mx-auto max-w-full lg:max-w-[460px] h-[600px] lg:h-[680px]">
+    <div className="relative w-full mx-auto max-w-full lg:max-w-[460px] h-[600px] lg:h-[760px]">
       <div
         ref={containerRef}
         role="group"
@@ -655,7 +655,7 @@ export default function VideoCard(props: VideoCardProps) {
         onKeyDown={handleKeyDown}
         tabIndex={0}
       >
-      <div className="relative w-full bg-black overflow-hidden h-[530px] lg:h-[600px]" style={{ borderRadius: "16px 16px 0 0" }}>
+      <div className="relative w-full bg-black overflow-hidden h-[530px] lg:h-[680px]" style={{ borderRadius: "16px 16px 0 0" }}>
         {videoSrc || src ? (
           <video
             ref={videoRef}
