291d2558476bbb5c067056f9cc2e7d5f1b56fb82
areja1 <abhinavreja2000@gmail.com>
2026-01-29 17:18:07 -0700

    Abhi/feature (#34)
    * Fixed the design
    
    ---------
    
    Co-authored-by: abhinav17reja <abhinav17reja@gmail.com>

--- components/FeedList.tsx
+++ components/FeedList.tsx
@@ -400,7 +400,7 @@ export default function FeedList({ activeTab, highlightPostId }: FeedListProps) 
         return (
           <section
             key={`${p.id}-${idx}`}
-            className="snap-start snap-always h-screen w-full flex items-start lg:items-center justify-center px-0 md:px-4 -mt-8 lg:mt-0"
+            className="snap-start snap-always h-screen w-full flex items-start lg:items-center justify-center px-0 md:px-4 mt-0"
          
             data-post-id={p.id}
             ref={(el) => {
@@ -412,7 +412,7 @@ export default function FeedList({ activeTab, highlightPostId }: FeedListProps) 
               }
             }}
           >
-            <div className="relative w-full h-full flex items-center justify-center max-w-full lg:-ml-80">
+            <div className="relative w-full h-full flex items-start lg:items-center justify-center max-w-full lg:-ml-80">
             <VideoCard
               // media
               src={p.video_url || undefined}
